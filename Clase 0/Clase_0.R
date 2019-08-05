65d00b5e644daa1842dae33395f0c66362076f72
Erick Perez <erickps309@gmail.com>
2019-08-05 15:41:16 -0500

    Script 0

---
+++ Clase 0/Clase_0.R
@@ -0,0 +1,27 @@
+# Erick Perez Silva
+# 05/08/2019
+# Clase 0
+
+
+# Pasos basicos -----------------------------------------------------------
+
+2+2
+a <- 2
+a*a
+a+5
+
+
+# Importar datos ----------------------------------------------------------
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+diametro
+
+#Medidas de tendencia central
+mean(diametro)
+median(diametro)
+
+#Medias de dispersion
+
+var(diametro)
+sd(diametro)
